e99dfe4643bb40145c9725c68d89a03d2313d3bd
Harper <wendy8564@gmail.com>
2024-11-16 00:47:45 +0800

    建立新專案

---
+++ src/main/java/tw/idv/harper/cathaybktest/CathaybkTestApplication.java
@@ -0,0 +1,13 @@
+package tw.idv.harper.cathaybktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathaybkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathaybkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/idv/harper/cathaybktest/CathaybkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.idv.harper.cathaybktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CathaybkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
